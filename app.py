0ab1dc2614278ed7da35d0e015a903c5f40930dd
ildizsigrai <ildi.zsigrai@gmail.com>
2024-07-02 11:52:46 +0200

    type conversion

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ print("hello " + name)
 birth_year = input("Enter your birth year: ")
 age = 2024 - int(birth_year)
 print("Age: ", age)
+
+# type conversions // CALCULATOR
+first = float(input("first:"))
+second = float(input("second: "))
+sum = first + second
+print("sum", str(sum))
